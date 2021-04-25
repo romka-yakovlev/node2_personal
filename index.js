b97246a3065290902313c22040cc1802b8e350dc
Romen <romka.asm@gmail.com>
2021-04-25 13:26:33 +0300

    Basic app skeleton

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello, my name is Roman and this is my simple web page");
+})
+
+app.listen(3000, () => {
+    console.log("hey, I'm listening for requests");
+})
